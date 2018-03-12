5951088dfc89d72b9ad818f4cf2b36a9d668f081
Raymond Cabrera <rjgcengr@gmail.com>
2018-03-12 11:13:19 -0700

    Complete all pseudoclassical subclass implementations

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,16 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+//setting prototype
+ForagerBee.prototype = Object.create(Bee.prototype);
+//setting constructor
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function() {
+  this.treasureChest.push('treasure');
+};
